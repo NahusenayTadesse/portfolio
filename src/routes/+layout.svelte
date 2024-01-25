8177b69c0d4be21ba7da0dd470e504a76fe24585
NahusenayTadesse <124673344+NahusenayTadesse@users.noreply.github.com>
2024-01-25 11:25:19 +0300

    Morning Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,48 +1,111 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte';
+	import homeLight from '@iconify/icons-iconamoon/home-light';
+	import { AppShell, AppBar, AppRailAnchor } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	let currentTile: number = 0;
+	let width = '50';
+	let iconClass = 'flex items-center justify-center';
 </script>
 
 <!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
+	<svelte:fragment slot="sidebarLeft">
 		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
-		</AppBar>
+		<AppRail>
+			<LightSwitch />
+			<div class="h-12" />
+			<AppRailAnchor href="/" title="Home">
+				<svelte:fragment slot="lead">
+					<div class={iconClass}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							class="w-12 h-12"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+							/>
+						</svg>
+					</div>
+				</svelte:fragment>
+				<span>Home</span>
+			</AppRailAnchor>
+
+			<AppRailAnchor href="/about-me" title="About Me">
+				<svelte:fragment slot="lead"
+					><div class={iconClass} />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						class="w-12 h-12"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+						/>
+					</svg>
+				</svelte:fragment>
+				<span>About Me</span>
+			</AppRailAnchor>
+
+			<AppRailAnchor href="/portfolio" title="Portfolio">
+				<svelte:fragment slot="lead"
+					><div class={iconClass} />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						class="w-12 h-12"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+						/>
+					</svg>
+				</svelte:fragment>
+				<span>Portfolio</span>
+			</AppRailAnchor>
+
+			<AppRailAnchor href="/contact" title="Contact">
+				<svelte:fragment slot="lead"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						class="w-12 h-12"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
+						/>
+					</svg>
+				</svelte:fragment>
+				<span>Contact</span>
+			</AppRailAnchor>
+		</AppRail>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="h1">Contact Me</h1>
